c42dc6374c74f1d05ae851c1dbeabaccbac5bebe
Ken <ken.kamijoh@gmail.com>
2017-06-11 04:15:50 +0000

    test commit3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, TEST world!"
+    render html: "hello, test world!"
   end
 end
